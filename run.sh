892d0c6bcbdd4f249e97c680bb04ceec8d7002d7
Owen Suphanat <owen0813715430@hotmail.com>
2024-05-15 10:02:44 +0700

    updated

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 export DATA_SOURCE_NAME=qwer
 export CUSTOM_METRICS=./dataguard-config.yaml
 
-./oracledb_exporter --custom.metrics ./dataguard-config.yaml\
-  --web.listen-address 9161 \
+./oracledb_exporter --custom.metrics "./dataguard-config.yaml"\
+  --web.listen-address --web.listen-address 0.0.0.0:9161 \
 
   
   
